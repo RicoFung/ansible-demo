782abb82d986e44431ee86428840e2605d7d96b0
zhuojun.feng <zhuojun.feng@moco.com>
2021-07-20 18:51:02 +0800

    202107201850 1.添加变量EV_BRANCH

--- uat/e3_core/shell/exe.sh
+++ uat/e3_core/shell/exe.sh
@@ -4,10 +4,12 @@
 OPERATION=$1
 # 参数 2 运行环境
 ENV=$2
-# 参数 3 工作目录
-BASE_DIR=$3
+# 参数 3 分支名
+BRANCH=$3
+# 参数 4 工作目录
+BASE_DIR=$4
 
 export IPOS_ANSIBLE=/data/epo-ansible/ipos-ansible
 cd ${IPOS_ANSIBLE}/ansible_scripts/${ENV} && git pull
 # 执行ansible操作
-ansible-playbook -i ${BASE_DIR}/hosts ${BASE_DIR}/playbooks/core.yml -f 1 --extra-vars "EV_OPERATION=$OPERATION EV_ENV=$ENV EV_IPOS_ANSIBLE=$IPOS_ANSIBLE"
+ansible-playbook -i ${BASE_DIR}/hosts ${BASE_DIR}/playbooks/core.yml -f 1 --extra-vars "EV_OPERATION=$OPERATION EV_ENV=$ENV EV_BRANCH=$BRANCH EV_IPOS_ANSIBLE=$IPOS_ANSIBLE"
