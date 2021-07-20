4d3943642bb5af1522ed45617620766d98a1e01c
zhuojun.feng <zhuojun.feng@moco.com>
2021-07-20 11:10:24 +0800

    202107201110 1.规范化变量命名

--- uat/e3_core/shell/exe.sh
+++ uat/e3_core/shell/exe.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # 参数 1 执行的操作 deploy
-operation=$1
-# 参数 2 工作目录
-BASE_DIR=$2
-# 参数 3 运行环境
-deploy_env=$3
+OPERATION=$1
+# 参数 2 运行环境
+ENV=$2
+# 参数 3 工作目录
+BASE_DIR=$3
 
-export ipos_ansible=/data/epo-ansible/ipos-ansible
-cd ${ipos_ansible}/ansible_scripts/${deploy_env} && git pull
+export IPOS_ANSIBLE=/data/epo-ansible/ipos-ansible
+cd ${IPOS_ANSIBLE}/ansible_scripts/${ENV} && git pull
 # 执行ansible操作
-ansible-playbook -i ${BASE_DIR}/hosts ${BASE_DIR}/playbooks/core.yml -f 1 --extra-vars "operation=$operation deploy_env=$deploy_env ipos_ansible=$ipos_ansible"
+ansible-playbook -i ${BASE_DIR}/hosts ${BASE_DIR}/playbooks/core.yml -f 1 --extra-vars "EV_OPERATION=$OPERATION EV_ENV=$ENV EV_IPOS_ANSIBLE=$IPOS_ANSIBLE"
