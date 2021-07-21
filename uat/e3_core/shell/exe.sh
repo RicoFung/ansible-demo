9217a79cebb3d5c29ef64051b838c0223f99838c
zhuojun.feng <zhuojun.feng@moco.com>
2021-07-21 15:30:52 +0800

    202107211530 [CORE] 1.调整core脚本

--- uat/e3_core/shell/exe.sh
+++ uat/e3_core/shell/exe.sh
@@ -10,6 +10,6 @@ BRANCH=$3
 BASE_DIR=$4
 
 export IPOS_ANSIBLE=/data/epo-ansible/ipos-ansible
-cd ${IPOS_ANSIBLE}/ansible_scripts/${ENV} && git pull
+cd ${IPOS_ANSIBLE}/ansible_scripts/${ENV} && git pull --force
 # 执行ansible操作
 ansible-playbook -i ${BASE_DIR}/hosts ${BASE_DIR}/playbooks/exe.yml -f 1 --extra-vars "EV_OPERATION=$OPERATION EV_ENV=$ENV EV_BRANCH=$BRANCH EV_IPOS_ANSIBLE=$IPOS_ANSIBLE"
